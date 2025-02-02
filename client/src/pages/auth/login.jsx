9543a7de3b8da148d5ec04350fd28c6e271fa4a4
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-02-02 02:22:19 -0700

    testing the landing page

---
+++ client/src/pages/auth/login.jsx
@@ -0,0 +1,10 @@
+
+
+
+const AuthLogin = () => {
+  return (
+    <h2>login</h2>
+  )
+}
+
+export default AuthLogin;
---
+++ client/src/pages/auth/register.jsx
@@ -0,0 +1,9 @@
+
+
+const AuthRegister = () => {
+  return (
+    <div>Register hoy ki na</div>
+  )
+}
+
+export default AuthRegister;
