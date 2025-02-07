f9a234962318b24d08beab3fb8e6e653f6a40a3d
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-02-07 03:46:48 -0700

    admin product system was added now on trail more work to do

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -3,8 +3,11 @@ import CommonForm from "@/components/common/form";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
 import { addProductFormElements } from "@/config";
+import { useToast } from "@/hooks/use-toast";
+import { addNewProduct, fetchAllProducts } from "@/store/admin/products-slice";
 
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 const initialFormData = {
   image: null,
   title: "",
@@ -23,14 +26,35 @@ const AdminProducts = () => {
     const [imageFile, setImageFile] = useState(null);
     const [uploadedImageUrl, setUploadedImageUrl] = useState("");
     const [imageLoadingState, setImageLoadingState] = useState(false);
+    const { productList } = useSelector((state) => state.adminProducts);
+    const dispatch=useDispatch();
+    const { toast } = useToast();
     //mysubmit
-    function onSubmit() {
-   
+    function onSubmit(event) {
+   event.preventDefault();
+   dispatch(
+    addNewProduct({
+      ...formData,
+      image: uploadedImageUrl,
+    })
+  ).then((data) => {
+    if (data?.payload?.success) {
+      dispatch(fetchAllProducts());
+      setOpenCreateProductsDialog(false);
+      setImageFile(null);
+      setFormData(initialFormData);
+      toast({
+        title: "Product add successfully",
+      });
+    }
+  });
     }
 
- console.log(formData,"formData");
+ console.log(productList,"productList",uploadedImageUrl);
  
-
+ useEffect(() => {
+  dispatch(fetchAllProducts());
+}, [dispatch]);
   return (
     <Fragment>
     <div className="mb-5 w-full flex justify-end">
